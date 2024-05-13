a6888ecb5dc636ddea0195d57fb3779a7e584aea
Koushik <koushikashok176@gmail.com>
2024-05-13 19:33:59 +0530

    Adding Websockets using socket.io

--- With REST API/Rest API Serverside code/app.js
+++ With REST API/Rest API Serverside code/app.js
@@ -71,6 +71,10 @@ mongoose
     "mongodb+srv://koushik176:EE8zpNRiXw8W8joA@cluster0.xpfqfkj.mongodb.net/messages?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then((result) => {
-    app.listen(8080);
+    const server = app.listen(8080);
+    const io = require("./socket").init(server);
+    io.on("connection", (socket) => {
+      console.log("Client Connected!");
+    });
   })
   .catch((err) => console.log(err));
